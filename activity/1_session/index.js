7a2a25f27ee6a9906fa12576b538377cef90c3e1
Sergio Aguilar <31091635+sdar270492@users.noreply.github.com>
2022-06-23 23:36:33 -0500

    resolución actividad 1
    resolución actividad 1

--- activity/1_session/index.js
+++ activity/1_session/index.js
@@ -1,14 +1,29 @@
+import {solucion} from './aboutme.js';
 // Ejercicio 1
-import "./aboutme.js";
+
+console.log(solucion.ejercicio1());
 console.log("Ejercicio 1");
 
 // Ejercicio 2
 
+console.log(solucion.ejercicio2());
+
 // Ejercicio 3
+
+console.log(solucion.ejercicio3());
 
 // Ejercicio 4
 
+console.log(solucion.ejercicio4(1,100));
+
 // Ejercicio 5
 
-// Ejercicio 6
+console.log(solucion.ejercicio5());
 
+// Ejercicio 6_1
+
+console.log(solucion.ejercicio6_1("world!"));
+
+// Ejercicio 6_2
+
+console.log(solucion.ejercicio6_2("I'm Lucas"));
